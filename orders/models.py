dcb32a516bc027890a645b6845b04c509e50c0da
delabere <jack.rickards@hotmail.co.uk>
2019-11-20 16:51:09 +0000

    added first set of models to models.py

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+# TODO: create models for: pizzas, toppings, subs, pasta, salads, dinner-platters
+
+class Pizza(models.Model):
+    pizza_type = models.CharField(max_length=64)
+    pizza_size = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.pizza_size} {self.pizza_type}'
+
+class PizzaToppings(models.Model):
+    topping = models.ManyToManyField(Pizza, blank=True, related_name='toppings')
+
+    def __str__(self):
+        return f'{self.topping}'
+
+
+class Subs(models.Model):
+    sub_type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.sub_type}'
+
+class SubToppings(models.Model):
+    topping = models.ManyToManyField(Subs, blank=True, related_name='toppings')
+
+    def __str__(self):
+        return f'{self.topping}'
+
+class Pasta(models.Model):
+    pasta = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.pasta}'
+
+class Platters(models.Model):
+    platter_type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.platter_type}'
