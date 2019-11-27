d61da08860c26e36cd8fcc148740c7e1f46e7b49
delabere <jack.rickards@hotmail.co.uk>
2019-11-27 16:37:10 +0000

    created and integrated fancy login view

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 # Create your views here.
+
+
 def index(request):
     if not request.user.is_authenticated:
-        return render(request, "orders/login.html", {"message": None})
+        return render(request, "orders/fancy_login.html", {"message": None})
     context = {
         "user": request.user
     }
@@ -16,7 +19,6 @@ def index(request):
 def login_view(request):
     username = request.POST["username"]
     password = request.POST["password"]
-    print(username, password)
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
@@ -24,6 +26,47 @@ def login_view(request):
     else:
         return render(request, "orders/login.html", {"message": "Invalid credentials."})
 
+
+def fancy_login_view(request):
+    username=request.POST["username"]
+    password=request.POST["password"]
+    user=authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/fancy_login.html", {"message": "Invalid credentials."})
+
+
 def logout_view(request):
     logout(request)
     return render(request, "orders/login.html", {"message": "Logged out."})
+
+
+def register_view(request):
+    # if not request.user.is_authenticated:
+        # return render(request, "orders/register.html", {"message": None})
+    if request.method == 'POST':
+
+
+        username=request.POST["username"]
+        password=request.POST["password"]
+        first_name=request.POST["first_name"]
+        last_name=request.POST["last_name"]
+        email=request.POST["email"]
+
+
+
+        user=User.objects.create_user(
+            username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+
+        user.save()
+
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "orders/register.html", {"message": "Invalid credentials."})
+
+    return render(request, "orders/register.html", {"message": None})
