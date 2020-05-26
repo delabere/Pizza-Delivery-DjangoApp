220e5941e30aaff3dc57b1b62608c1045cffa0a0
Jack <jack.rickards@hotmail.co.uk>
2020-05-26 14:11:30 +0000

    allowed hosts

--- jackspizza/pizza/settings.py
+++ jackspizza/pizza/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i0&iq&e9u9h6(4_7%pt2s9)f=c$kso=k$c$w@fi9215s=1q0^d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["jackspizza.co.uk", "www.jackspizza.co.uk"]
+ALLOWED_HOSTS = ["jackspizza.co.uk", "www.jackspizza.co.uk", "www.delabere.com", "pizza.delabere.com", "161.35.32.198"]
 
 
 # Application definition
